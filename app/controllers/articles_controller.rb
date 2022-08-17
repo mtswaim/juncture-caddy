bdecf427cbe722c1b04090b3185e6d9f1fe27af4
Mathew Swaim <mathewswaim@Hiltis-MBP.attlocal.net>
2022-08-16 20:13:05 -0500

    Created the 'create' method for creating an article
    defined the method
    returned JSON and 201 if it has all required fields
    return 400 if not

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,2 +1,17 @@
 class ArticlesController < ApplicationController
+  # POST /articles
+  def create
+    @article = Article.new(article_params)
+    if @article.save
+      render json: @article, status: 201
+    else
+      render error: { error: 'Unable to create article' }, status: 400
+    end
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :content, :author, :category, :published_at)
+  end
 end
